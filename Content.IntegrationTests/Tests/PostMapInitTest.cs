f0a8b6964e26c50822015937241b2fbac6a6e8c9
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2025-07-27 18:11:57 +0300

    New Starting Pod (#20)
    * Rename the drop pod
    
    * Don't require all jobs to have spawners
    
    * Colony drop pod
    
    * My rennie <3
    
    * Tweaks
    
    * Fix what the merge broke
    
    * why did it not save this change
    
    * Update planet-colony-drop-pod.yml
    
    * BecomesStation
    
    * add a mapped multitool
    
    * Update colony-drop-pod.yml
    
    * Update atmos on the drop pod

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -70,7 +70,8 @@ namespace Content.IntegrationTests.Tests
             "Fland", // Floof,
             "Amber", // Apparently, floof?
             "Europa",
-            "PlanetContinental" // Vulp
+            "PlanetContinentalBoubaLandingShuttle", // Vulp
+            "PlanetContinentalColonyDropPod", // Vulp
         };
 
         /// <summary>
@@ -274,7 +275,8 @@ namespace Content.IntegrationTests.Tests
                             jobs.Remove(jobId);
                     }
 
-                    Assert.That(jobs, Is.Empty, $"There is no spawnpoints for {string.Join(", ", jobs)} on {mapProto}.");
+                    // Vulpstation - fuck you, this was never necessary
+                    // Assert.That(jobs, Is.Empty, $"There is no spawnpoints for {string.Join(", ", jobs)} on {mapProto}.");
                 }
 
                 try
