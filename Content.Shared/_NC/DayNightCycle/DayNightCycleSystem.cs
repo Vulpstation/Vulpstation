fc733ad45110d43a0120d78f3dca3bd66fe81d44
Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
2025-07-31 22:33:21 +0300

    Planet Station Initial FTL (#37)
    * Planet station initial FTL + cleanup
    
    * No randomized landing angle
    
    * Fixes
    
    * Fix custom D/N cycle not working due to client-side prediction
    
    * More proper D/N cycle

--- Content.Shared/_NC/DayNightCycle/DayNightCycleSystem.cs
+++ Content.Shared/_NC/DayNightCycle/DayNightCycleSystem.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Robust.Shared.Map.Components;
+using Robust.Shared.Network;
 using Robust.Shared.Timing;
 
 namespace Content.Shared._NC14.DayNightCycle
@@ -7,6 +8,7 @@ namespace Content.Shared._NC14.DayNightCycle
     public sealed class DayNightCycleSystem : EntitySystem
     {
         [Dependency] private readonly IGameTiming _gameTiming = default!;
+        [Dependency] private readonly INetManager _netMan = default!; // Vulpstation
 
         private const float EARLY_MORNING_TIME = 0.2f; // This represents 20% into the cycle, which is early morning
 
@@ -57,6 +59,9 @@ namespace Content.Shared._NC14.DayNightCycle
 
         public override void Update(float frameTime)
         {
+            if (_netMan.IsClient) // Vulpstation - horrible, absolutely terrible. Why is this in shared?!
+                return;
+
             base.Update(frameTime);
 
             var query = EntityQueryEnumerator<DayNightCycleComponent, MapLightComponent>();
